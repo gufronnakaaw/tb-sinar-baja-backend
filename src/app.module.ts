42b1a1354f06629e1f875c6e83d38554fc2c6653
gufronnakaaw <gufronnakaaw@gmail.com>
2024-05-08 19:40:32 +0700

    delete: owner, admin, and cashier module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
-import { CashierModule } from './cashier/cashier.module';
-import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [OwnerModule, AdminModule, CashierModule],
+  imports: [],
   controllers: [AppController],
   providers: [],
 })
--- src/cashier/cashier.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CashierService } from './cashier.service';
-
-@Controller('cashier')
-export class CashierController {
-  constructor(private readonly cashierService: CashierService) {}
-}
