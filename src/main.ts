bcf0e20d9f49fa5f73a0520cf7652e262d1fa1d8
gufronnakaaw <gufronnakaaw@gmail.com>
2024-05-11 19:52:57 +0700

    add: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './utils/global/global.exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api', {
     exclude: ['/'],
