c20fee625b8bd15607d8f42b58bb9db048fa3bfb
gufronnakaaw <gufronnakaaw@gmail.com>
2024-05-13 11:24:50 +0700

    add: kategori and gudang module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { KategoriModule } from './kategori/kategori.module';
 import { ProdukModule } from './produk/produk.module';
 import { TransaksiModule } from './transaksi/transaksi.module';
+import { GudangModule } from './gudang/gudang.module';
 
 @Module({
-  imports: [ProdukModule, TransaksiModule],
+  imports: [ProdukModule, TransaksiModule, KategoriModule, GudangModule],
   controllers: [AppController],
   providers: [],
 })
