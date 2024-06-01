62589689798ca8f23005641ee81d5f12da8a4c7e
gufronnakaaw <gufronnakaaw@gmail.com>
2024-06-01 20:46:20 +0700

    create: update and delete member api

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,15 +1,23 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   UsePipes,
 } from '@nestjs/common';
 import { SuccessResponse } from '../utils/global/global.response';
 import { ZodValidationPipe } from '../utils/pipes/zod.pipe';
-import { CreateMemberDto, createMemberSchema } from './member.dto';
+import {
+  CreateMemberDto,
+  UpdateMemberDto,
+  createMemberSchema,
+  updateMemberSchema,
+} from './member.dto';
 import { MemberService } from './member.service';
 
 @Controller('member')
@@ -39,6 +47,37 @@ export class MemberController {
         success: true,
         status_code: HttpStatus.CREATED,
         data: await this.memberService.createMember(body),
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Patch()
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(new ZodValidationPipe(updateMemberSchema))
+  async update(@Body() body: UpdateMemberDto): Promise<SuccessResponse> {
+    try {
+      return {
+        success: true,
+        status_code: HttpStatus.OK,
+        data: await this.memberService.updateMember(body),
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete(':id_member')
+  @HttpCode(HttpStatus.OK)
+  async destroy(
+    @Param('id_member') id_member: string,
+  ): Promise<SuccessResponse> {
+    try {
+      return {
+        success: true,
+        status_code: HttpStatus.OK,
+        data: await this.memberService.deleteMember(id_member),
       };
     } catch (error) {
       throw error;
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,6 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../utils/services/prisma.service';
-import { CreateMemberDto } from './member.dto';
+import { CreateMemberDto, UpdateMemberDto } from './member.dto';
 
 @Injectable()
 export class MemberService {
@@ -82,6 +86,80 @@ export class MemberService {
         alamat: body.alamat,
         email: body.email,
         no_telp: body.no_telp,
+      },
+      select: {
+        id_member: true,
+        nama: true,
+        perusahaan: true,
+        alamat: true,
+        email: true,
+        no_telp: true,
+        created_at: true,
+        updated_at: true,
+        level: {
+          select: {
+            nama: true,
+          },
+        },
+      },
+    });
+  }
+
+  async updateMember(body: UpdateMemberDto) {
+    const member = await this.prisma.member.count({
+      where: {
+        id_member: body.id_member,
+      },
+    });
+
+    if (!member) {
+      throw new NotFoundException('Member tidak ditemukan');
+    }
+
+    return this.prisma.member.update({
+      where: {
+        id_member: body.id_member,
+      },
+      data: {
+        level_id: body.level_id,
+        nama: body.nama,
+        perusahaan: body.perusahaan,
+        alamat: body.alamat,
+        email: body.email,
+        no_telp: body.no_telp,
+      },
+      select: {
+        id_member: true,
+        nama: true,
+        perusahaan: true,
+        alamat: true,
+        email: true,
+        no_telp: true,
+        created_at: true,
+        updated_at: true,
+        level: {
+          select: {
+            nama: true,
+          },
+        },
+      },
+    });
+  }
+
+  async deleteMember(id_member: string) {
+    const member = await this.prisma.member.count({
+      where: {
+        id_member,
+      },
+    });
+
+    if (!member) {
+      throw new NotFoundException('Member tidak ditemukan');
+    }
+
+    return this.prisma.member.delete({
+      where: {
+        id_member,
       },
       select: {
         id_member: true,
