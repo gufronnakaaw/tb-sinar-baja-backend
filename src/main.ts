e0d391dfffa3da6e7cb2238408866ca3a4ce1889
gufronnakaaw <gufronnakaaw@gmail.com>
2024-09-06 15:41:59 +0700

    update: port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './utils/global/global.exception';
 
+function getPort(mode: string) {
+  if (mode == 'production') {
+    return 3434;
+  }
+
+  if (mode == 'development') {
+    return 3535;
+  }
+
+  if (mode == 'final') {
+    return 2424;
+  }
+
+  if (mode == 'online') {
+    return 2626;
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
@@ -13,6 +31,6 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(HttpAdapterHost)));
-  await app.listen(process.env.MODE == 'production' ? 3434 : 3535);
+  await app.listen(getPort(process.env.MODE));
 }
 bootstrap();
